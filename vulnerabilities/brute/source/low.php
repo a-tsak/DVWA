95b8c078d0bbdbc2c08894a9e0d0e7a63fed3a91
a-tsak <62567253+a-tsak@users.noreply.github.com>
2022-12-27 20:22:03 +0200

    Update low.php

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -9,7 +9,7 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = md5( $pass );
 
 	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
+	$query = "SELECT * FROM users WHERE login = :login AND pass = :pass";
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
